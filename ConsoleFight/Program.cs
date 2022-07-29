7b53462d7f7d4d083bdb25980bd98168540ba5bc
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-07-29 16:54:33 +0300

    Add project files.

---
+++ ConsoleFight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
